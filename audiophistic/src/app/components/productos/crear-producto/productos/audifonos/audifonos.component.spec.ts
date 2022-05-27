7886fc2cee408bde5b8c2a8db4c8db69f3ef8bcf
Josué Gerardo <44277078+josuegerardo96@users.noreply.github.com>
2022-05-26 20:53:55 -0600

    Error, se subieron cambios al archivo principal y así no era :sad:

--- audiophistic/src/app/components/productos/crear-producto/productos/audifonos/audifonos.component.spec.ts
+++ audiophistic/src/app/components/productos/crear-producto/productos/audifonos/audifonos.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AudifonosComponent } from './audifonos.component';
+import { AudifonosFormComponent } from './audifonos.component';
 
-describe('AudifonosComponent', () => {
-  let component: AudifonosComponent;
-  let fixture: ComponentFixture<AudifonosComponent>;
+describe('AudifonosFormComponent', () => {
+  let component: AudifonosFormComponent;
+  let fixture: ComponentFixture<AudifonosFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AudifonosComponent ]
+      declarations: [ AudifonosFormComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AudifonosComponent);
+    fixture = TestBed.createComponent(AudifonosFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
